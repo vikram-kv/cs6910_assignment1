73b80aa9bad61e649b7351453d2899f99232fb7f
vikram-kv <cs19b021@smail.iitm.ac.in>
2023-02-13 20:35:19 +0530

    fixed act functions to make it simpler; added list of arguments in main.py; on the process of correcting vanilla neural network to adopt instructor's convention - to ensure correctness

--- activation_functions.py
+++ activation_functions.py
@@ -1,57 +1,32 @@
 import numpy as np
 
-class ActivationFunction():
-    def value(self, *args):
-        pass
-    def derivative(self, *args):
-        pass
-    @property
-    def name(self):
-        pass
+# sigmoid activation function and derivative
+def sigmoid_value(x):
+    return 1.0/(1.0 + np.exp(-x))
 
-class Sigmoid(ActivationFunction):
-    @property
-    def name(self):
-        return 'sigmoid'
-    @staticmethod
-    def value(x):
-        return 1.0/(1.0 + np.exp(-x))
-    @staticmethod
-    def derivative(x):
-        fx = Sigmoid.value(x)
-        return (1.0 - fx) * fx
+def sigmoid_deriv(x):
+    fx = sigmoid_value(x)
+    return (1.0 - fx) * fx
 
-class ReLu(ActivationFunction):
-    @property
-    def name(self):
-        return 'relu'
-    @staticmethod
-    def value(x):
-        return (0.0 if x < 0 else x)
-    @staticmethod
-    def derivative(x):
-        return (0.0 if x < 0 else 1.0)
+# relu activation function and derivative
+def relu_value(x):
+    return (0.0 if x < 0 else x)
 
-class TanH(ActivationFunction):
-    @property
-    def name(self):
-        return 'tanh'
-    @staticmethod
-    def value(x):
-        y, z = np.exp(x), np.exp(-x)
-        return (y - z)/(y + z)
-    @staticmethod
-    def derivative(x):
-        fx = TanH.value(x)
-        return (1.0 - fx) * (1.0 + fx)
+def relu_deriv(x):
+    return (0.0 if x < 0 else 1.0)
 
-class Linear(ActivationFunction):
-    @property
-    def name(self):
-        return 'linear'
-    @staticmethod
-    def value(x):
-        return x
-    @staticmethod
-    def derivative(x):
-        return 1.0
+# tanh activation function and derivative
+def tanh_value(x):
+    y, z = np.exp(x), np.exp(-x)
+    return (y-z)/(y+z)
+
+def tanh_deriv(x):
+    fx = tanh_value(x)
+    return (1.0 - fx) * (1.0 + fx)
+
+# linear activation function and derivative
+def linear_value(x):
+    return x
+
+def linear_deriv(x):
+    return 1.0
---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+from activation_functions import *
+import argparse as ap
+import wandb
+
+# change default values to reflect best results later
+parser = ap.ArgumentParser(description='Multi-Layer Feedfoward Neural Network with Various Optimizers')
+parser.add_argument('-wp','--wandb_project',dest='wdp', default='cs6910-assignment1', help='Project name used to track experiments in Weights & Biases dashboard')
+parser.add_argument('-we', '--wandb_entity',dest='wde', default='vikram-kv', help='Wandb Entity used to track experiments in the Weights & Biases dashboard')
+parser.add_argument('-d', '--dataset', dest='dataset', default='fashion_mnist', choices=['fashion_mnist', 'mnist'], help='Dataset to be used')
+parser.add_argument('-e', '--epochs', dest='epochs', default=1, type=int, help='Number of epochs to train neural network')
+parser.add_argument('-b', '--batch_size', dest='batch_size', default=4, type=int, help='Batch size used to train neural network')
+parser.add_argument('-l', '--loss', dest='loss', default='cross_entropy', choices=['mean_squared_error', 'cross_entropy'], help='Loss function to be used for training')
+parser.add_argument('-o', '--optimizer', dest='opt', default='sgd', choices=['sgd', 'momentum', 'nag', 'rmsprop', 'adam', 'nadam'], help='Optimization method to use for training')
+parser.add_argument('-lr','--learning_rate', dest='lr', default=0.1, type=float, help='Learning rate used to optimize model parameters')
+parser.add_argument('-m', '--momentum',dest='mom', default=0.5, type=float, help='Momentum used by momentum and nag optimizers')
+parser.add_argument('-beta', '--beta', dest='beta', default=0.5, type=float, help='Beta used by rmsprop optimizer')
+parser.add_argument('-beta1', '--beta1', dest='beta1', default=0.5, type=float, help='Beta1 used by adam and nadam optimizers.')
+parser.add_argument('-beta2', '--beta2', dest='beta2', default=0.5, type=float, help='Beta2 used by adam and nadam optimizers.')
+parser.add_argument('-eps', '--epsilon', dest='eps', default=0.000001, type=float, help='Epsilon used by optimizers')
+parser.add_argument('-w_d', '--weight_decay', dest='w_d', default=.0, type=float, help='Weight decay used by optimizers')
+parser.add_argument('-w_i', '--weight_init', dest='w_i', default='random', choices=['random', 'Xavier'], help='Weight initialization method to be used')
+parser.add_argument('-nhl', '--num_layers', dest='num_layers', default=1, type=int, help='Number of hidden layers used in feedforward neural network')
+parser.add_argument('-sz', '--hidden_size', dest='hidden_size', default=4, type=float, help='Number of hidden neurons in a feedforward layer')
+parser.add_argument('-a', '--activation', dest='act_func', default='sigmoid', choices=['identity', 'sigmoid', 'tanh', 'ReLU'], help='Activation function to be used')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    
